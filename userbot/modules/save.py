d9761327d33a43b9ce4b98a41def11dcbeee8665
mrismanaziz <mrismanaziz@gmail.com>
2021-04-10 03:48:21 +0000

    pylint: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/save.py
+++ userbot/modules/save.py
@@ -5,12 +5,15 @@
 from userbot import CMD_HELP, bot
 from userbot.events import register
 
+
 @register(outgoing=True, pattern="^.save(?: |$)(.*)", disable_errors=True)
 async def saf(e):
     x = await e.get_reply_message()
     if not x:
         return await eod(
-            e, "Balas ke Pesan Apa Saja untuk menyimpannya ke pesan simpanan Anda", time=5
+            e,
+            "Balas ke Pesan Apa Saja untuk menyimpannya ke pesan simpanan Anda",
+            time=5,
         )
     await bot.send_message("me", x)
     await eod(e, "Message saved at saved messages", time=5)
