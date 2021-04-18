a0adad5ca29866288ebe7b1d58ce36deff8f063c
mrismanaziz <mrismanaziz@gmail.com>
2021-04-18 07:44:09 +0000

    pylint: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/core.py
+++ userbot/modules/core.py
@@ -1,14 +1,11 @@
-import importlib
-import logging
-import sys
 import os
 from pathlib import Path
 
-from userbot.utils import load_module
-from userbot import CMD_HELP, LOGS, bot
 from userbot.events import register
+from userbot.utils import load_module
 
 DELETE_TIMEOUT = 5
+
 
 @register(outgoing=True, pattern=r"^\.install$")
 async def _(event):
@@ -17,15 +14,21 @@ async def _(event):
     if event.reply_to_msg_id:
         try:
             await event.edit("`Installing Modules...`")
-            downloaded_file_name = await event.client.download_media(  # pylint:disable=E0602
-                await event.get_reply_message(),
-                "userbot/modules/"  # pylint:disable=E0602
+            downloaded_file_name = (
+                await event.client.download_media(  # pylint:disable=E0602
+                    await event.get_reply_message(),
+                    "userbot/modules/",  # pylint:disable=E0602
+                )
             )
             if "(" not in downloaded_file_name:
                 path1 = Path(downloaded_file_name)
                 shortname = path1.stem
                 load_module(shortname.replace(".py", ""))
-                await event.edit("Berhasil Menginstall Plugin `{}`".format(os.path.basename(downloaded_file_name)))
+                await event.edit(
+                    "Berhasil Menginstall Plugin `{}`".format(
+                        os.path.basename(downloaded_file_name)
+                    )
+                )
             else:
                 os.remove(downloaded_file_name)
                 await event.edit("Errors! Plugin ini sebelumnya sudah terinstall...")
@@ -47,9 +50,8 @@ async def send(event):
         the_plugin_file,
         force_document=True,
         allow_cache=False,
-        reply_to=message_id
+        reply_to=message_id,
     )
     end = datetime.now()
     time_taken_in_ms = (end - start).seconds
     await event.edit("Diupload {} di {} detik".format(input_str, time_taken_in_ms))
-
